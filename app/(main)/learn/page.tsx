c44974996c927d566451bdf59f7dceeb57f6adf1
Jefferson <jefferson.brandao@g.globo>
2024-03-16 02:35:32 -0300

    configurando neon pg e drizzle orm

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import FeedWrapper from "@/components/shared/feed-wrapper";
+import StickyWrapper from "@/components/shared/sticky-wrapper";
+import React from "react";
+import Header from "./header";
+import UserProgress from "@/components/shared/user-progress";
 
 const LearnPage = () => {
   return (
-    <div>LearnPage</div>
-  )
-}
+    <div className="flex gap-[48px] px-6">
+      <FeedWrapper>
+        <Header title="Espanha" />
+      </FeedWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Espanha", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+    </div>
+  );
+};
 
-export default LearnPage
+export default LearnPage;
