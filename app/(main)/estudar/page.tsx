1e6386d8a3e42bf708b61a12193def9baaa4338e
Jefferson <jefferson.brandao@g.globo>
2024-03-18 14:21:06 -0300

    criando query de units

--- app/(main)/estudar/page.tsx
+++ app/(main)/estudar/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import FeedWrapper from "@/components/shared/feed-wrapper";
 import StickyWrapper from "@/components/shared/sticky-wrapper";
 import Header from "./header";
@@ -7,8 +7,12 @@ import UserProgress from "@/components/shared/user-progress";
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/cursos");
@@ -18,6 +22,11 @@ const LearnPage = async () => {
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
       <StickyWrapper>
         <UserProgress
--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs";
-import { courses, userProgress } from "./schema";
+import { courses, lessons, units, userProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth()
@@ -17,6 +17,39 @@ export const getUserProgress = cache(async () => {
   })
 
   return data
+})
+
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress()
+
+  if (!userProgress?.activeCourseId) return []
+
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: true
+            }
+          }
+        }
+      }
+    }
+  })
+
+  const normalizeData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return challenge.challengeProgress && challenge.challengeProgress.length > 0 && challenge.challengeProgress.every((progress) => progress.completed)
+      })
+      return { ...lesson, completed: allCompletedChallenges }
+    })
+    return { ...unit, lessons: lessonsWithCompletedStatus }
+  })
+
+  return normalizeData
 })
 
 export const getCourses = cache(async () => {
